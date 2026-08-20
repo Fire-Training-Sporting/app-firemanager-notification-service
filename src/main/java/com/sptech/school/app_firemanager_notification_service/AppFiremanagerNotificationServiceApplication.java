46ac6f7689be229d398c19eaebfa2bc659ccf12c
Luand-Assis <luan.assis@sptech.school>
2026-08-19 22:38:13 -0300

    initial repository

---
+++ src/main/java/com/sptech/school/app_firemanager_notification_service/AppFiremanagerNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sptech.school.app_firemanager_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFiremanagerNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFiremanagerNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sptech/school/app_firemanager_notification_service/AppFiremanagerNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptech.school.app_firemanager_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFiremanagerNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
