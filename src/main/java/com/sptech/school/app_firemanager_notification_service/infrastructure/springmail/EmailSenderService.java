4811a6f786d3cc376756b77d90576a5e58ad035a
Luand-Assis <luan.assis@sptech.school>
2026-08-23 00:56:12 -0300

    feat: email service para conexao com servidor smtp

---
+++ src/main/java/com/sptech/school/app_firemanager_notification_service/infrastructure/springmail/EmailSenderService.java
@@ -0,0 +1,41 @@
+package com.sptech.school.app_firemanager_notification_service.infrastructure.springmail;
+
+import com.sptech.school.app_firemanager_notification_service.application.interfaces.saida.EmailSenderInterface;
+import com.sptech.school.app_firemanager_notification_service.application.models.Email;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderInterface {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailRemetente;
+
+    public EmailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void enviar(Email email) {
+
+        try {
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mensagem, true, "UTF-8");
+
+            helper.setFrom(emailRemetente);
+            helper.setTo(email.destinatario());
+            helper.setSubject(email.assunto());
+            helper.setText(email.corpoMensagem(), true);
+
+            javaMailSender.send(mensagem);
+        } catch (MessagingException e) {
+            System.out.println(e);
+        }
+    }
+}
