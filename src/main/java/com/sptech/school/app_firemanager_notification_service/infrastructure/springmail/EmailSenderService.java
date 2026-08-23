880d8fb389f7bcfede922c99a7fbadb25e9b1601
Luand-Assis <luan.assis@sptech.school>
2026-08-23 16:39:39 -0300

    feat: habilitando async para servico nao bloqueante i/o

--- src/main/java/com/sptech/school/app_firemanager_notification_service/infrastructure/springmail/EmailSenderService.java
+++ src/main/java/com/sptech/school/app_firemanager_notification_service/infrastructure/springmail/EmailSenderService.java
@@ -7,6 +7,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +23,7 @@ public class EmailSenderService implements EmailSenderInterface {
     }
 
     @Override
+    @Async
     public void enviar(Email email) {
 
         try {
